7c27919ba4c190619fadf1476776766a7cdf2373
shogo <0814.sho.mura@gmail.com>
2025-03-28 18:28:32 +0900

    add all models to admin.py

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Product,CartItem,Order,OrderItem
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(CartItem)
+admin.site.register(OrderItem)
+admin.site.register(Order)
